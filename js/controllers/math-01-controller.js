ce7afdf10d34bd3a76bc627558e9fc38aaaf8688
Dragos Silion <silidragos@gmail.com>
2021-03-29 09:41:54 +0300

    Chemistry

--- js/controllers/math-01-controller.js
+++ js/controllers/math-01-controller.js
@@ -1,7 +1,7 @@
-class Math01Controller {
+class Math01Controller extends StepsController{
     constructor() {
+        super();
         let geos = document.getElementsByClassName("geo-set");
-        this.idx = 0;
         this.elements = [{
             startTime: 0,
             endTime: 32.0,
@@ -80,75 +80,6 @@ class Math01Controller {
             base_url + "math/architecture_1_montage.mp4",
             base_url + "math/architecture_1_montage_2.mp4"
         ];
-        this.currentVideo = 0;
-        this.isVideoReset = true;
-        this.video = document.getElementById('video');
-
-
-        const video = this.video;
-        document.addEventListener('keyup', event => {
-          if (event.code === 'Space') {
-            this.OnNextStep();
-          }
-        })
-        video.addEventListener(
-            'timeupdate',
-            () => {
-                if(this.isVideoReset && video.currentTime >= this.elements[this.idx].endTime){
-                    let oldIdx = this.idx;
-                    this.idx += 1;
-                    this.SetStep(this.elements[oldIdx], this.elements[this.idx]);
-                }
-            });
-        video.addEventListener(
-            'ended',
-            ()=>{
-                if(!this.isVideoReset) {
-                    console.log("Video resetted because finished.");
-                    this.isVideoReset = true;
-                    this.SetNextVideo();
-                }
-            }
-        )
-    }
-    SetStep(oldEl, newEl, isManuallySet = false){
-        if(newEl.paused){
-            this.video.pause();
-            document.getElementById("video-control").setAttribute("visible", false);
-        }else{
-            this.video.play();
-            document.getElementById("video-control").setAttribute("visible", true);
-        }
-        if(!isManuallySet && newEl.isNextVideo){
-            this.isVideoReset = false;
-        }
-
-        if(oldEl !== null && oldEl.elementToUnhide !== null){
-            oldEl.elementToUnhide.setAttribute("visible", false);
-        }
-        if(newEl.elementToUnhide !== null){
-            newEl.elementToUnhide.setAttribute("visible", true);
-        }
-    }
-
-    OnNextStep() {
-        let oldIdx = this.idx;
-        this.idx++;
-        console.log("Next", this.idx);
-        if(this.elements[this.idx].isNextVideo !== undefined && this.elements[this.idx].isNextVideo){
-            this.SetNextVideo();
-        }
-        this.video.currentTime = this.elements[this.idx].startTime;
-        this.SetStep(this.elements[oldIdx], this.elements[this.idx], true);
-    }
-
-    SetNextVideo(){
-        if(this.currentVideo === this.videos.length) return;
-        video.src = this.videos[this.currentVideo++];
-        video.play();
-        video.volume = 0;
-
-        document.dispatchEvent(new Event("skyboxUpdated"));
     }
 }
 
---
+++ js/controllers/steps-controller.js
@@ -0,0 +1,85 @@
+class StepsController {
+    constructor() {
+        this.idx = 0;
+        this.elements = [{
+            startTime: 0,
+            endTime: 32.0,
+            elementToUnhide: null,
+            paused: false,
+            isNextVideo: true
+        }];
+        this.videos = [
+            ""
+        ];
+
+        this.currentVideo = 0;
+        this.isVideoReset = true;
+        this.video = document.getElementById('video');
+
+
+        const video = this.video;
+        document.addEventListener('keyup', event => {
+          if (event.code === 'Space') {
+            this.OnNextStep();
+          }
+        })
+        video.addEventListener(
+            'timeupdate',
+            () => {
+                if(this.isVideoReset && video.currentTime >= this.elements[this.idx].endTime){
+                    let oldIdx = this.idx;
+                    this.idx += 1;
+                    this.SetStep(this.elements[oldIdx], this.elements[this.idx]);
+                }
+            });
+        video.addEventListener(
+            'ended',
+            ()=>{
+                if(!this.isVideoReset) {
+                    console.log("Video resetted because finished.");
+                    this.isVideoReset = true;
+                    this.SetNextVideo();
+                }
+            }
+        )
+    }
+    SetStep(oldEl, newEl, isManuallySet = false){
+        if(newEl.paused){
+            this.video.pause();
+            document.getElementById("video-control").setAttribute("visible", false);
+        }else{
+            this.video.play();
+            document.getElementById("video-control").setAttribute("visible", true);
+        }
+        if(!isManuallySet && newEl.isNextVideo){
+            this.isVideoReset = false;
+        }
+
+        if(oldEl !== null && oldEl.elementToUnhide !== null){
+            oldEl.elementToUnhide.setAttribute("visible", false);
+        }
+        if(newEl.elementToUnhide !== null){
+            newEl.elementToUnhide.setAttribute("visible", true);
+        }
+    }
+
+    OnNextStep() {
+        let oldIdx = this.idx;
+        this.idx++;
+        console.log("Next", this.idx);
+        if(this.elements[this.idx].isNextVideo !== undefined && this.elements[this.idx].isNextVideo){
+            this.SetNextVideo();
+        }
+        this.video.currentTime = this.elements[this.idx].startTime;
+        this.SetStep(this.elements[oldIdx], this.elements[this.idx], true);
+    }
+
+    SetNextVideo(){
+        if(this.currentVideo === this.videos.length) return;
+        video.src = this.videos[this.currentVideo++];
+        video.play();
+        video.volume = 0;
+
+        document.dispatchEvent(new Event("skyboxUpdated"));
+    }
+}
